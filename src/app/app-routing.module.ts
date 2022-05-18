ffdf5b52bbdbbd7961a70e82c94cdb36f575dffc
Aper-ux <alex.quiroga.perez@gmail.com>
2022-05-18 15:55:23 -0400

    Routing actualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactsComponent } from './contacts/contacts.component';
 import { HomeComponent } from './home/home.component';
 import { ProdsComponent } from './prods/prods.component';
 
@@ -11,6 +13,14 @@ const routes: Routes = [
   {
     path:"prods",
     component:ProdsComponent
+  },
+  {
+    path:"clients",
+    component:ClientsComponent
+  },
+  {
+    path:"contacts",
+    component:ContactsComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProdsComponent } from './prods/prods.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProdsComponent
+    ProdsComponent,
+    ClientsComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
